cd9b608e827d3c5a6515fd0a733702c7ba43dfd6
Maria Mercedes <mariamercedes@Marias-Air.lan>
2024-06-12 20:55:05 -0400

    finalized homework

---
+++ app.js
@@ -0,0 +1,58 @@
+/*alert("Hello World!")
+console.log("Hello")*/
+
+/*const x = 2;
+const y = 3;
+console.log(x + y);*/
+
+//console.log("Hello World!")
+
+/*function displayAlert() {
+    console.log("Hello World!")
+}
+
+// displayAlert()*/
+
+/*let x="hello world";
+let mynumber = 2
+let thisisadog= true
+let mycar = 'Toyota'
+mycar = 'Honda'
+console.log(typeof mynumber)
+
+let thisIsNull = null;
+let thisIsUndefined = undefined;
+let thisIsaNumber =1;
+let thisIsaString = 'hello';
+let thisIsaBoolea = true;
+
+console.log(typeof thisIsUndefined);*/
+
+/*let NumberofCows = 40;
+let RandomString ='Hello World';
+let var1= 'Hello';
+let var2=' Disney Land';
+let result= var1.concat(var2);
+console.log(result);
+console.log("He/llo, "+ "Disney Land")*/
+
+/*const firstname = "Maria";
+const lastname = "Mercedes";
+
+console.log("This is my name: " + firstname + " " + lastname)
+
+const firsnumber = 8;
+const secondnumber = 20;
+
+console.log(`this is my name: ${firstname} ${lastname} and I am ${firsnumber + secondnumber} years old`);
+
+console.log(5-2);
+console.log(5+2);
+console.log(5/2);
+console.log(5%2); // remainer between 2 numbers, how many times the second number goes into first*/
+
+let a = true; let b= false;
+let answer = a || b;
+let answer2 = a && b;
+console.log(answer);
+console.log(answer2);
---
+++ workshop.js
@@ -0,0 +1,15 @@
+//Creatign string for user regarding secret combination
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+//creating the variables that make up my combination resulting from my own caculations
+const variable1 = 5+5;
+const variable2 = 4*10;
+const variable3 = 40-1;
+const combination = `${variable1} - ${variable2} - ${variable3}`;
+
+//creatign the alert for the user with secret combination
+alert(`The combination of the vault is: ${combination}`);
+
+//displaying combination
+console.log(`The combination of the vault is:" ${combination}`);
+
